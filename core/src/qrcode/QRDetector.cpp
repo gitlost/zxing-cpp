d4c6bff5941e3e870dcef4297768e487dedf1ba8
gitlost <burmartke@gmail.com>
2023-05-17 10:43:48 +0100

    WIP

--- core/src/qrcode/QRDetector.cpp
+++ core/src/qrcode/QRDetector.cpp
@@ -85,7 +85,7 @@ std::vector<ConcentricPattern> FindFinderPatterns(const BitMatrix& image, bool t
 					log(*pattern - PointF(.2, 0), 3);
 					log(*pattern + PointF(0, .2), 3);
 					log(*pattern - PointF(0, .2), 3);
-					//assert(image.get(pattern->x, pattern->y));
+					assert(image.get(pattern->x, pattern->y));
 					res.push_back(*pattern);
 				}
 			}

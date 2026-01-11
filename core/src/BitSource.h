be0a2772da7e15981f20ad67398db7c92da34653
gitlost <burmartke@gmail.com>
2026-01-11 23:27:22 +0000

    Typo peakBits() -> peekBits()

--- core/src/BitSource.h
+++ core/src/BitSource.h
@@ -57,11 +57,11 @@ public:
 	int readBits(int numBits);
 
 	/**
-	* @param numBits number of bits to peak
-	* @return int representing the bits peaked.  The bits will appear as the least-significant
+	* @param numBits number of bits to peek
+	* @return int representing the bits peeked.  The bits will appear as the least-significant
 	*         bits of the int
 	*/
-	int peakBits(int numBits) const;
+	int peekBits(int numBits) const;
 
 	/**
 	* @return number of bits that can be read successfully
